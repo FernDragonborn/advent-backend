931268c74a5b4cb347d46b2dcda891fbb5e5bf7d
Fern Dragonborn <FernDragonborn@gmail.com>
2024-11-26 18:21:08 +0200

    Update: static files

--- advent_backend/settings.py
+++ advent_backend/settings.py
@@ -151,6 +151,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "src"), # Django will search for /src/
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
